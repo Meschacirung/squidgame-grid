417af0eab38ca713c6f4f30638daa3f700252cf4
MLHocalhost <18cy039@esisalama.org>
2021-10-13 22:57:00 +0200

    feat (ui) : create responsive grid

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: [],
+  mode : ['jit'],
+  purge: ['./**/*.html'],
   presets: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
